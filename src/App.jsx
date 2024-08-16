35569bfd8680006b1b82f98991a79c6193fa5713
Gaurav Kumar <gaurav989267@gmail.com>
2024-08-16 17:48:03 -0400

    create app

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import { Button } from "@material-tailwind/react";
+function App() {
+
+
+  return (
+    <>
+      <div className='w-screen h-screen bg-white'>
+      <Button>Button</Button>
+      </div>
+
+    </>
+  )
+}
+
+export default App
