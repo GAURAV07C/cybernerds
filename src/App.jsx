89842ce15a7bab768e1a198b3df9627a610f4cc2
Gaurav Kumar <gaurav989267@gmail.com>
2024-08-17 12:03:51 -0400

    adding bootstrap

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,16 @@
 import "./App.css";
 import { Button } from "@material-tailwind/react";
+
 function App() {
   return (
     <>
-      <div className="w-full h-full flex flex-col bg-white">
-        <div className=" h-screen w-11/12  mx-auto flex flex-col justify-center items-center">
-          <p className="text-blue-gray-500 text-2xl">
+      <div className="">
+        <div className=" ">
+          <p className="">
             This app use tech stack - react + talwind + MUi
           </p>
           <div>
-          <Button className="text-2xl bg-blue-gray-500">Button </Button>
+          <Button className="">Button </Button>
           <Button>hii</Button>
           </div>
 
